78118a0cb27675c35312461116698d2b3380c040
Anton Sizikov <anton.sizikov@pm.me>
2023-01-07 21:26:22 +0100

    count measure time, less logging for now

--- src/Organizer.Application/Services/WorkflowService.cs
+++ src/Organizer.Application/Services/WorkflowService.cs
@@ -8,15 +8,18 @@ namespace Organizer.Application.Services;
 public class WorkflowService : IWorkflowService
 {
     private readonly IApplicationDbContext context;
+
     public WorkflowService(IApplicationDbContext context)
     {
         this.context = context;
     }
-    
+
     public async Task RunAsync()
     {
         Console.WriteLine("WorkflowService.RunAsync");
+        var stopwatch = Stopwatch.StartNew();
         await StartReadingFilesAsync();
+        Console.WriteLine($"WorkflowService.RunAsync: {stopwatch.ElapsedMilliseconds} ms");
     }
 
     private async Task StartReadingFilesAsync()
@@ -28,40 +31,48 @@ public class WorkflowService : IWorkflowService
             SingleReader = false
         });
         var writer = channel.Writer;
-        InitFileDataExtractors(channel);
-        var directory = @"/Users/asizikov/Downloads";
-        // var directory = @"D:\yadisk_photos_full_dump\YandexDisk\Фотокамера_copy";
+        var extractorTasks = InitFileDataExtractors(channel);
+        var directory = @"D:\yadisk_photos_full_dump\YandexDisk\Фотокамера_copy";
         foreach (var filePath in Directory.EnumerateFiles(directory))
         {
             await writer.WriteAsync(filePath);
-            // Console.WriteLine(filePath);
-            // context.PhotoFiles.Add(new PhotoFile
-            // {
-            //     Id = Guid.NewGuid(),
-            //     FileName = Path.GetFileName(filePath),
-            //     FileExtension = Path.GetExtension(filePath),
-            //     FilePath = filePath
-            // });
-            // await context.SaveChangesAsync(CancellationToken.None);
         }
+
         writer.Complete();
+
+        await Task.WhenAll(extractorTasks);
     }
 
-    private async Task InitFileDataExtractors(Channel<string> channel)
+    private List<Task> InitFileDataExtractors(Channel<string> channel)
     {
+        var tasks = new List<Task>();
         for (var i = 0; i < 10; i++)
         {
-            _ = Task.Factory.StartNew( async () =>
-            {
-                var id = i;
-                while (await channel.Reader.WaitToReadAsync()) 
+            tasks.Add(
+                Task.Factory.StartNew(async () =>
                 {
-                    if (channel.Reader.TryRead(out var filePath)) 
+                    var taskId = Guid.NewGuid();
+                    Console.WriteLine($"Data Extractor {taskId} started");
+                    var counter = 0L;
+                    while (await channel.Reader.WaitToReadAsync())
                     {
-                        Console.WriteLine(id + "--" + filePath);
+                        if (channel.Reader.TryRead(out var filePath))
+                        {
+                            context.PhotoFiles.Add(new PhotoFile
+                            {
+                                Id = Guid.NewGuid(),
+                                FileName = Path.GetFileName(filePath),
+                                FileExtension = Path.GetExtension(filePath),
+                                FilePath = filePath
+                            });
+                            await context.SaveChangesAsync(CancellationToken.None);
+                            counter++;
+                        }
                     }
-                }
-            });
+                    Console.WriteLine($"Data Extractor {taskId} finished, inserted {counter} records");
+                }));
         }
+
+        return tasks;
     }
 }
